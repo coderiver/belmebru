9781703e5e4901d9c57b921eb5a3a8ee09adc0e7
Splintex <eugene.godin@gmail.com>
2014-05-14 13:13:23 +0300

    1-0-catalog.html

--- js/common.js
+++ js/common.js
@@ -111,4 +111,70 @@ head.ready(function() {
 		event.stopPropagation();
 	});
 
+  	// 1. Initialize fotorama manually.
+    var $fotoramaDiv = $('.js-fotorama').on('fotorama:ready ',
+            function (e, fotorama, extra) {
+              $('.js-fotorama').addClass("is-visible");
+            }
+        )
+        // Initialize fotorama manually
+        .fotorama();
+
+    // 2. Get the API object.
+    var fotorama = $fotoramaDiv.data('fotorama');
+	$(".js-prev-banner").on("click", function(){
+		fotorama.show('<');
+		return false;
+	});
+	$(".js-next-banner").on("click", function(){
+		fotorama.show('>');
+		return false;
+	});
+
+	$("body").prepend( '<div class="tooltip js-tooltip"><div class="tooltip__in"></div></div>' );
+    var tooltip = $(".js-tooltip");
+    $(".js-tooltip-key").hover(
+        function(){
+            var left = $(this).offset().left;
+            var bottom = $(window).height() - $(this).offset().top;
+            var tooltip_html = $(this).attr("data-title");
+            tooltip.css({
+                left: left,
+                bottom: bottom-8 
+            });
+            tooltip.find(".tooltip__in").html(tooltip_html).fadeIn("fast");
+            tooltip.fadeIn("fast");
+        },
+        function() {
+            tooltip.hide();
+        }
+    );
+    tooltip.hover(
+        function(){
+            tooltip.show();
+        },
+        function() {
+            tooltip.hide(); 
+        }
+    );
+
+    if ($(".js-counter").length) {
+    	$(".js-counter").each(function(){
+    		var year = $(this).attr("data-year");
+	    	var month = $(this).attr("data-month")-1;
+	    	var day = $(this).attr("data-day");
+	    	var hour = $(this).attr("data-hour");
+	    	var minute = $(this).attr("data-minute");
+	        $(this).countdown({
+	            until: new Date(year, month, day, hour, minute),
+	            format: 'DHMS',
+	            compact: true,
+	            padZeroes: true,
+	            layout: '{d<}<div class="counter__item"><span>{dn}:</span></div>{d>}{h<}<div class="counter__item"><span>{hn}:</span></div>{h>}' + 
+	            '{m<}<div class="counter__item"><span>{mn}:</span></div>{m>}{s<}<div class="counter__item"><span>{sn}</span></div>{s>}'
+	        });
+    	});
+	    	
+    }
+
 });
--- js/init.js
+++ js/init.js
@@ -1,1 +1,1 @@
-head.load("js/lib/jquery.js","js/lib/jquery.validate.js", "js/common.js");
+head.load("js/lib/jquery.js","js/lib/fotorama.js","js/lib/jquery.validate.js","js/lib/jquery.countdown.js", "js/common.js");
