e1904fb57726de157182d3bd994a68e851a1cbe9
Splintex <eugene.godin@gmail.com>
2014-05-21 16:50:19 +0300

    news, otzyvy.html

--- js/common.js
+++ js/common.js
@@ -4,8 +4,8 @@ head.ready(function() {
 		$(".js-overlay").hide();
 		$("body").removeClass("has-open-popup");
 		$(".js-select-list").hide();
-        $(".js-select").removeClass("is-active");
-	});  
+		$(".js-select").removeClass("is-active");
+	});	
 
 	// validation form
 	$(".js-validate").each(function(){
@@ -104,26 +104,26 @@ head.ready(function() {
 		event.stopPropagation();
 		return false;
 	});
-  	$(".js-popup-close").on("click", function(){
-  		$(".js-overlay").fadeOut(200); 
-  		$("body").removeClass("has-open-popup")
+	$(".js-popup-close").on("click", function(){
+		$(".js-overlay").fadeOut(200); 
+		$("body").removeClass("has-open-popup")
 		return false;
 	});
-  	$(".js-popup").on("click", function(event){
+	$(".js-popup").on("click", function(event){
 		event.stopPropagation();
 	});
 
- //  	// 1. Initialize fotorama manually.
- //    var $fotoramaDiv = $('.js-fotorama').on('fotorama:ready ',
- //            function (e, fotorama, extra) {
- //              $('.js-fotorama').addClass("is-visible");
- //            }
- //        )
- //        // Initialize fotorama manually
- //        .fotorama();
+ //		// 1. Initialize fotorama manually.
+ //		var $fotoramaDiv = $('.js-fotorama').on('fotorama:ready ',
+ //						function (e, fotorama, extra) {
+ //							$('.js-fotorama').addClass("is-visible");
+ //						}
+ //				)
+ //				// Initialize fotorama manually
+ //				.fotorama();
 
- //    // 2. Get the API object.
- //    var fotorama = $fotoramaDiv.data('fotorama');
+ //		// 2. Get the API object.
+ //		var fotorama = $fotoramaDiv.data('fotorama');
 	// $(".js-prev-banner").on("click", function(){
 	// 	fotorama.show('<');
 	// 	return false;
@@ -134,52 +134,52 @@ head.ready(function() {
 	// });
 
 	$("body").prepend( '<div class="tooltip js-tooltip"><div class="tooltip__in"></div></div>' );
-    var tooltip = $(".js-tooltip");
-    $(".js-tooltip-key").hover(
-        function(){
-            var left = $(this).offset().left;
-            var bottom = $(window).height() - $(this).offset().top;
-            var tooltip_html = $(this).attr("data-title");
-            tooltip.css({
-                left: left,
-                bottom: bottom-8 
-            });
-            tooltip.find(".tooltip__in").html(tooltip_html).fadeIn("fast");
-            tooltip.fadeIn("fast");
-        },
-        function() {
-            tooltip.hide();
-        }
-    );
-    tooltip.hover(
-        function(){
-            tooltip.show();
-        },
-        function() {
-            tooltip.hide(); 
-        }
-    );
+	var tooltip = $(".js-tooltip");
+	$(".js-tooltip-key").hover(
+		function(){
+			var left = $(this).offset().left;
+			var bottom = $(window).height() - $(this).offset().top;
+			var tooltip_html = $(this).attr("data-title");
+			tooltip.css({
+				left: left,
+				bottom: bottom-8 
+			});
+			tooltip.find(".tooltip__in").html(tooltip_html).fadeIn("fast");
+			tooltip.fadeIn("fast");
+		},
+		function() {
+			tooltip.hide();
+		}
+	);
+	tooltip.hover(
+		function(){
+			tooltip.show();
+		},
+		function() {
+			tooltip.hide(); 
+		}
+	);
 
-    if ($(".js-counter").length) {
-    	$(".js-counter").each(function(){
-    		var year = $(this).attr("data-year");
-	    	var month = $(this).attr("data-month")-1;
-	    	var day = $(this).attr("data-day");
-	    	var hour = $(this).attr("data-hour");
-	    	var minute = $(this).attr("data-minute");
-	        $(this).countdown({
-	            until: new Date(year, month, day, hour, minute),
-	            format: 'DHMS',
-	            compact: true,
-	            padZeroes: true,
-	            layout: '{d<}<div class="counter__item"><span>{dn}:</span></div>{d>}{h<}<div class="counter__item"><span>{hn}:</span></div>{h>}' + 
-	            '{m<}<div class="counter__item"><span>{mn}:</span></div>{m>}{s<}<div class="counter__item"><span>{sn}</span></div>{s>}'
-	        });
-    	});
-	    	
-    }
+	if ($(".js-counter").length) {
+		$(".js-counter").each(function(){
+			var year = $(this).attr("data-year");
+			var month = $(this).attr("data-month")-1;
+			var day = $(this).attr("data-day");
+			var hour = $(this).attr("data-hour");
+			var minute = $(this).attr("data-minute");
+			$(this).countdown({
+				until: new Date(year, month, day, hour, minute),
+				format: 'DHMS',
+				compact: true,
+				padZeroes: true,
+				layout: '{d<}<div class="counter__item"><span>{dn}:</span></div>{d>}{h<}<div class="counter__item"><span>{hn}:</span></div>{h>}' + 
+				'{m<}<div class="counter__item"><span>{mn}:</span></div>{m>}{s<}<div class="counter__item"><span>{sn}</span></div>{s>}'
+			});
+		});
+			
+	}
 
-    $('.js-slider-banner').slick({
+	$('.js-slider-banner').slick({
 		slidesToShow: 1,
 		infinite: false,
 		speed: 300,
@@ -209,141 +209,177 @@ head.ready(function() {
 	$('.js-scroll-pane').jScrollPane();
 
 
-    function ui_slider_range() {
-        $(".js-ui-slider-range").each(function(){
-            var slider = $(this).find(".js-ui-slider-main");
-            var input_from = $(this).find(".js-ui-slider-from");
-            var input_to = $(this).find(".js-ui-slider-to");
-            var min_val = +$(this).attr("data-min");
-            var max_val = +$(this).attr("data-max");
-            slider.slider({
-                range: true,
-                min: min_val,
-                max: max_val,
-                step: 1000,  
-                values: [ min_val, max_val ],
-                slide: function( event, ui ) {
-                    $(this).find(".ui-slider-handle").html("<span></span>");
-                    var handle_0 = $(this).find(".ui-slider-range").next().find("span")
-                    var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
-                    input_from.text(ui.values[0]);
-                    input_to.text(ui.values[1]);
-                    handle_0.text(ui.values[0]);
-                    handle_1.text(ui.values[1]);
-                }
-            });
-            console.log(handle_0);
-            console.log(handle_1);
-            $(this).find(".ui-slider-handle").html("<span></span>");
-            var handle_0 = $(this).find(".ui-slider-range").next().find("span")
-            var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
-            handle_0.text(slider.slider( "values", 0 ));
-            handle_1.text(slider.slider( "values", 1 ));
-            input_from.text(slider.slider( "values", 0 ));
-            input_to.text(slider.slider( "values", 1 ));
-        });
-    }
-    ui_slider_range();
+	function ui_slider_range() {
+		$(".js-ui-slider-range").each(function(){
+			var slider = $(this).find(".js-ui-slider-main");
+			var input_from = $(this).find(".js-ui-slider-from");
+			var input_to = $(this).find(".js-ui-slider-to");
+			var min_val = +$(this).attr("data-min");
+			var max_val = +$(this).attr("data-max");
+			slider.slider({
+				range: true,
+				min: min_val,
+				max: max_val,
+				step: 1000,	
+				values: [ min_val, max_val ],
+				slide: function( event, ui ) {
+					$(this).find(".ui-slider-handle").html("<span></span>");
+					var handle_0 = $(this).find(".ui-slider-range").next().find("span")
+					var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
+					input_from.text(ui.values[0]);
+					input_to.text(ui.values[1]);
+					handle_0.text(ui.values[0]);
+					handle_1.text(ui.values[1]);
+				}
+			});
+			console.log(handle_0);
+			console.log(handle_1);
+			$(this).find(".ui-slider-handle").html("<span></span>");
+			var handle_0 = $(this).find(".ui-slider-range").next().find("span")
+			var handle_1 = $(this).find(".ui-slider-range").next().next().find("span");
+			handle_0.text(slider.slider( "values", 0 ));
+			handle_1.text(slider.slider( "values", 1 ));
+			input_from.text(slider.slider( "values", 0 ));
+			input_to.text(slider.slider( "values", 1 ));
+		});
+	}
+	ui_slider_range();
 
-    $("body").on("click",".js-sort a", function(){
-    	if ($(this).hasClass("is-top-sort")) {
-    		$(this).removeClass("is-top-sort").addClass("is-down-sort");
-    	}
-    	else {
-    		$(".js-sort a").removeClass("is-top-sort").removeClass("is-down-sort");
-    		$(this).addClass("is-top-sort");
-    	}
+	$("body").on("click",".js-sort a", function(){
+		if ($(this).hasClass("is-top-sort")) {
+			$(this).removeClass("is-top-sort").addClass("is-down-sort");
+		}
+		else {
+			$(".js-sort a").removeClass("is-top-sort").removeClass("is-down-sort");
+			$(this).addClass("is-top-sort");
+		}
 		return false;
 	});
 
-    function choose() {
-        var number = $(".js-choose");
-        number.each(function(){
-            var max_number = +($(this).attr("data-max-number"));
-            var input = $(this).find("input");
-            var plus = $(this).find(".js-plus");
-            var minus = $(this).find(".js-minus");
-            plus.on("click", function(){
-                var val = +(input.val());
-                if (val >= max_number) {
-                    return false
+	function choose() {
+		var number = $(".js-choose");
+		number.each(function(){
+			var max_number = +($(this).attr("data-max-number"));
+			var input = $(this).find("input");
+			var plus = $(this).find(".js-plus");
+			var minus = $(this).find(".js-minus");
+			plus.on("click", function(){
+				var val = +(input.val());
+				if (val >= max_number) {
+					return false
+				}
+				else {
+					val += 1;
+					input.val(val);
+				}
+			});
+			minus.on("click", function(){
+				var val = +(input.val());
+				if (val > 1) {
+					val -= 1;
+					input.val(val);
+				}
+				else {
+					return false;
+				}
+			});
+		});
+	}
+	choose();
+
+	$(".js-del-row").on("click", function(){
+		if ($(".js-basket").find("tbody tr").length >= 2) {
+			$(this).parents("tr").addClass("is-remove-ready");
+			setTimeout(function(){
+				$("tr.is-remove-ready").remove();
+			}, 200);
+		}
+		else {
+			$(".js-basket").remove();
+			$(".js-basket-action").remove();
+			$(".js-basket-message").removeAttr("hidden");
+			//alert();
+		}
+
+	});
+
+	$(".js-reset-search").on("click", function(){
+		$(this).parents(".js-search").removeClass("is-active");
+		$(this).parents(".js-search").find(".js-search-input").attr("value","").focus();
+
+	});
+
+	ymaps.ready(init);
+		var myMap, 
+			myPlacemark;
+
+		function init(){ 
+			myMap = new ymaps.Map ("map", {
+				center: [40.743776, -73.992807],
+				zoom: 12,
+				controls: ['smallMapDefaultSet']
+			});
+		}
+
+	//rating
+	if ($('.js-rating').length > 0) {
+		$('.js-rating').each(function(){
+			var text = $(this).parent().find(".js-rating-text");
+			$(this).raty({
+				width: 120,
+				target: text,
+				targetText: '',
+				targetKeep : true,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star.png',
+				starOn : 'img/star-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+			
+	 }
+
+	if ($('.js-rating-read').length > 0) {
+		$('.js-rating-read').each(function(){
+			var text = $(this).parent().find(".js-rating-text");
+			$(this).raty({
+				width: 120,
+				readOnly: true,
+				target: text,
+				targetText: '',
+				targetKeep : true,
+				hints: ['Отвратительно', 'Плохо', 'Неплохо', 'Очень хорошо', 'Отлично'],
+				starOff: 'img/star.png',
+				starOn : 'img/star-act.png',
+				score: function() {
+					return $(this).attr('data-score');
+				}
+			});
+		});
+	}
+
+	if ($(".js-fancybox").length) {
+        $(".js-fancybox").fancybox({
+            openEffect  : 'elastic',
+            closeEffect : 'elastic',
+            nextEffect : 'none',
+            prevEffect : 'none',
+            padding: 23,
+            helpers: {
+                overlay: {
+                  locked: false
                 }
-                else {
-                    val += 1;
-                    input.val(val);
-                }
-            });
-            minus.on("click", function(){
-                var val = +(input.val());
-                if (val > 1) {
-                    val -= 1;
-                    input.val(val);
-                }
-                else {
-                    return false;
-                }
-            });
+            },
+            beforeShow: function() {
+                //$("body").addClass("has-open-popup");   
+            },
+            afterClose: function() {
+                //$("body").removeClass("has-open-popup");  
+            },
+            
         });
     }
-    choose();
-
-    $(".js-del-row").on("click", function(){
-    	if ($(".js-basket").find("tbody tr").length >= 2) {
-    		$(this).parents("tr").addClass("is-remove-ready");
-	    	setTimeout(function(){
-	    		$("tr.is-remove-ready").remove();
-	    	}, 200);
-    	}
-	    else {
-	    	$(".js-basket").remove();
-	    	$(".js-basket-action").remove();
-	    	$(".js-basket-message").removeAttr("hidden");
-	    	//alert();
-	    }
-
-    });
-
-    $(".js-reset-search").on("click", function(){
-    	$(this).parents(".js-search").removeClass("is-active");
-    	$(this).parents(".js-search").find(".js-search-input").attr("value","").focus();
-
-    });
-
-    ymaps.ready(init);
-        var myMap, 
-            myPlacemark;
-
-        function init(){ 
-            myMap = new ymaps.Map ("map", {
-                center: [40.743776, -73.992807],
-                zoom: 12,
-                controls: ['smallMapDefaultSet']
-            });
-          //   myPlacemark1 = new ymaps.Placemark([40.748296, -74.004394], 
-	         //    {
-	         //        hintContent: '',
-	         //        balloonContent: '<div class="map-info" style="width:322px;"><div class="map-info__inner"><a class="map-info__img" href="#"><img src="img/map-img.jpg"><div class="map-info__time">Круглосуточно</div><div class="map-info__badge"><i  class="icon icon-user-small"></i>6 +</div></a><div class="map-info__main"><a class="map-info__title" href="#">George</a><p>Первый премиальный грузинский ресторан в Москве</p><div class="map-info__meta"><span><i class="icon icon-point"></i>м. Братиславская</span><span><i class="icon icon-like-s"></i> 854</span></div></div></div>',
-	         //    }
-	         //    //{	
-	         //    //    iconImageHref: 'img/marker.png',
-	        	// //	iconImageSize: [45, 49],
-	        	// //	iconImageOffset: [-15, -35]
-	         //    //}
-          //   );
-          //   myPlacemark2 = new ymaps.Placemark([40.742963, -73.992335], 
-	         //    {
-	         //        hintContent: '', 
-	         //        balloonContent: '<div class="map-info" style="width:322px;"><div class="map-info__inner"><a class="map-info__img" href="#"><img src="img/map-img.jpg"><div class="map-info__time">Круглосуточно</div><div class="map-info__badge"><i  class="icon icon-user-small"></i>6 +</div></a><div class="map-info__main"><a class="map-info__title" href="#">George</a><p>Первый премиальный грузинский ресторан в Москве</p><div class="map-info__meta"><span><i class="icon icon-point"></i>м. Братиславская</span><span><i class="icon icon-like-s"></i> 854</span></div></div></div>',
-	         //    }
-	         //    //{	
-	         //    //    iconImageHref: 'img/marker.png',
-	        	// //	iconImageSize: [45, 49],
-	        	// //	iconImageOffset: [-15, -35]
-	         //    //}
-          //   );
-            
-            //myMap.geoObjects.add(myPlacemark1);
-            //myMap.geoObjects.add(myPlacemark2);
-        }
 
 });
