0c74260028e4c36686f52d0497174398aee0767f
Splintex <eugene.godin@gmail.com>
2014-06-23 10:31:50 +0300

    upd

--- js/init.js
+++ js/init.js
@@ -1,2 +1,2 @@
-head.load("js/lib/jquery.js","js/lib/jquery-ui-1.10.3.custom.js","js/lib/jquery.validate.js","js/lib/jquery.maskedinput.js","js/lib/jquery.countdown.js","js/lib/slick.min.js","js/lib/jquery.mousewheel.js","js/lib/jquery.jscrollpane.min.js","js/lib/jquery.raty.js","js/lib/jquery.fancybox.js", "js/common.js");
+head.load("js/lib/jquery.js","js/lib/jquery-ui-1.10.3.custom.js","js/lib/jquery.validate.js","js/lib/jquery.maskedinput.js","js/lib/jquery.countdown.js","js/lib/slick.min.js","js/lib/jquery.mousewheel.min.js","js/lib/jquery.jscrollpane.min.js","js/lib/jquery.raty.js","js/lib/jquery.fancybox.js", "js/common.js");
   
